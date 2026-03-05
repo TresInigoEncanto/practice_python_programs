7dd76d37a716f9728bab2e7d59c95c2739a40680
Tres <encantotresinigo@gmail.com>
2026-03-05 21:28:14 +0800

    tested gui

--- basketball_gamescore_with_gui.py
+++ basketball_gamescore_with_gui.py
@@ -1,1 +1,9 @@
-print("Hello")
+from tkinter import *
+
+WINDOW = Tk()
+WINDOW.geometry("420x420")
+WINDOW.title("Basketball Gamescore Calculator")
+
+WINDOW.config(background='orange')
+
+WINDOW.mainloop()
