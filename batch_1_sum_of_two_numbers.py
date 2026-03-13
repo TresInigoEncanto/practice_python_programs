d156e5575ad98933dad6ac957e0ac7b19998900f
Tres <encantotresinigo@gmail.com>
2026-03-13 22:22:42 +0800

    added a third variable for the sum, then revised the code to print the sum of the numbers

---
+++ batch_1_sum_of_two_numbers.py
@@ -0,0 +1,5 @@
+first_number = float(input("Enter your first number: "))
+second_number = float(input("Enter your second number: "))
+sum_of_numbers = first_number + second_number
+
+print(f"The sum of the numbers are {sum_of_numbers:.2f}")
