fbd21421fa5dfc823ff7e146e5518cc980862eba
Tres <encantotresinigo@gmail.com>
2026-03-13 22:32:53 +0800

    added the functions needed to find the product of the numbers

---
+++ batch_1_product_of_two_numbers.py
@@ -0,0 +1,5 @@
+first_number = float(input("Enter your first number: "))
+second_number = float(input("Enter your second number: "))
+product_of_numbers = first_number * second_number
+
+print(f"The product of the numbers is {product_of_numbers:.2f}")
