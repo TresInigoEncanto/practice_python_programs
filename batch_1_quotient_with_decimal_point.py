e869f137beeab333b0d2b961ebe82761cb3c7dd5
Tres <encantotresinigo@gmail.com>
2026-03-13 22:37:20 +0800

    Added the functions needed to find the quotient of the 2 numbers

---
+++ batch_1_quotient_with_decimal_point.py
@@ -0,0 +1,5 @@
+first_number = float(input("Enter your first number: "))
+second_number = float(input("Enter your second number: "))
+quotient_of_numbers = first_number / second_number
+
+print(f"The quotient of the numbers is {quotient_of_numbers:.2f}")
