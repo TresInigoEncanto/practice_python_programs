d5fd2fe7bd65d745f3d129aa6fc244533a3a4d25
Tres <encantotresinigo@gmail.com>
2026-03-13 22:44:08 +0800

    Added the function needed for raising the first input to the second input

---
+++ batch_1_exponent_of_numbers.py
@@ -0,0 +1,5 @@
+first_number = float(input("Enter your first number: "))
+second_number = float(input("Enter your second number: "))
+exponent_of_numbers = first_number ** second_number
+
+print(f"The result of the exponentials is {exponent_of_numbers:.2f}")
