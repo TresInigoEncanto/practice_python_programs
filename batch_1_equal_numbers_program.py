8ad8c84e799fcdc29aa688ae165ce44f2eea5bc8
Tres <encantotresinigo@gmail.com>
2026-03-13 22:15:25 +0800

    added a function when numbers are equal

---
+++ batch_1_equal_numbers_program.py
@@ -0,0 +1,9 @@
+first_number = float(input("Enter your first number: "))
+second_number = float(input("Enter your second number: "))
+
+if first_number > second_number:
+    print(f"The first number is bigger = {first_number:.2f}")
+elif first_number < second_number:
+    print(f"The second number is bigger = {second_number:.2f}")
+else:
+    print(f"The numbers", first_number, "and", second_number, "are equal")
