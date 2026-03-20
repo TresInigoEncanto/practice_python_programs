ccdec0f505e441e1a53b5438bb45434282eaa84e
Tres <encantotresinigo@gmail.com>
2026-03-20 14:25:43 +0800

    Added a running total and used it as a subtrahend for the first number to get its difference

--- batch_2_difference_of_ten_numbers.py
+++ batch_2_difference_of_ten_numbers.py
@@ -1,4 +1,11 @@
 first_number = float(input("Enter Value 1: "))
 
+running_total = 0
+
 for index in range (1,10):
-    float(input(f"Enter value {index + 1}: "))
+    remaining_numbers = float(input(f"Enter value {index + 1}: "))
+    running_total += remaining_numbers
+
+difference = float(first_number - running_total)
+
+print(f"The difference of the first number and the nine numbers is {difference:.2f}")
