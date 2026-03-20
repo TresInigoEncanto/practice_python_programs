fc2231bef70d1a45267e91663c4925261e1ff551
Tres <encantotresinigo@gmail.com>
2026-03-20 13:33:21 +0800

    Added an elif function to print equal indication of numbers

---
+++ batch_2_print_not_equal_numbers.py
@@ -0,0 +1,9 @@
+first_number = float(input("Enter your first number: "))
+second_number = float(input("Enter your second number: "))
+
+if first_number > second_number:
+    print("The numbers are not equal")
+elif second_number > first_number:
+    print("The numbers are not equal")
+else:
+    print("The numbers are equal")
