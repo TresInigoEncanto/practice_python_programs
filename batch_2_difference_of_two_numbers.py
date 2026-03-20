4e16000d8d33d1a3c6c3cd22751275c79784ac58
Tres <encantotresinigo@gmail.com>
2026-03-20 13:38:46 +0800

    Added a difference variable to print the difference of the two numbers

---
+++ batch_2_difference_of_two_numbers.py
@@ -0,0 +1,6 @@
+first_number = float(input("Enter your first number: "))
+second_number = float(input("Enter your second number: "))
+difference = first_number - second_number
+
+print(f"The difference of the two numbers is {difference:.2f}")
+
