7229c526458d1f0e79b3b8cb300c56dd3c177f20
Tres <encantotresinigo@gmail.com>
2026-03-20 13:44:10 +0800

    Added a variable to be able to print the quotient of the numbers

---
+++ batch_2_quotient_without_decimal.py
@@ -0,0 +1,6 @@
+first_number = float(input("Enter your first number: "))
+second_number = float(input("Enter your second number: "))
+quotient = int(first_number // second_number)
+
+print("The quotient of the two numbers is", quotient)
+
