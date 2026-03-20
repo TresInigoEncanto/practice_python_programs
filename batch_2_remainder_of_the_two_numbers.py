f5c4c5773821b1ed96a22d0a9b8cefcb1eb8fbb1
Tres <encantotresinigo@gmail.com>
2026-03-20 13:48:13 +0800

    Added a modulo operator to find the remainder of the numbers

---
+++ batch_2_remainder_of_the_two_numbers.py
@@ -0,0 +1,5 @@
+first_number = float(input("Enter your first number: "))
+second_number = float(input("Enter your second number: "))
+remainder = int(first_number % second_number)
+
+print("The remainder of the two numbers is", remainder)
