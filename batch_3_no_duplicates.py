8701be26aaa8a7c78c3568273a615d91e543eab5
Tres <encantotresinigo@gmail.com>
2026-03-24 00:30:29 +0800

    Added a list to store the unique numbers that will be entered, then used the append() function to remove the numbers entered that will be processed by the second for loop. The second for loop will organize the entered numbers and if it counted a number that was entered more than once, it will not print it.

--- batch_3_no_duplicates.py
+++ batch_3_no_duplicates.py
@@ -1,2 +1,11 @@
+unique_numbers = []
+
 for index in range (0,10):
     numbers = float(input(f"Enter value {index + 1}: "))
+    unique_numbers.append(numbers)
+
+print("Unique numbers:")
+
+for numbers in unique_numbers:
+    if unique_numbers.count(numbers) == 1:
+        print(numbers)
