b3dcd619d183f3978d607c9903edb1e43fe97de5
Tres <encantotresinigo@gmail.com>
2026-06-07 00:05:04 +0800

    Recreated ljust() function by creating a variable converting the width of the input into an integer. Created a variable to get the current length of the user input. Created a variable to hold the final padded string

---
+++ batch_6_ljust_function.py
@@ -0,0 +1,9 @@
+user_input = input("Enter string: ")
+width_input = input("Enter desired width: ")
+fill_char = input("Enter [0] to pad with the string: ")
+
+target_width = int(width_input)
+
+current_len = len(user_input)
+
+padded_text = user_input
