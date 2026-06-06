b2220cae6ece12373da2561f21ad19bd39959800
Tres <encantotresinigo@gmail.com>
2026-06-06 23:56:52 +0800

    Added an if-else statement to check if the suffix is longer than the text. Created a for loop to compare the user input characters to the characters of the suffix to find. Created two variables to calculate matching positions from the end of the index. Created an if statement to see if the characters match or not

--- batch_6_endswith_function.py
+++ batch_6_endswith_function.py
@@ -6,4 +6,18 @@ suffix_len = len(find_suffix)
 
 ends_with_suffix = True
 
+if suffix_len > text_len:
+    ends_with_suffix = False
+
+else:
+    for i in range(suffix_len):
+        text_char = user_input[text_len - 1 - i]
+        suffix_char = find_suffix[suffix_len - 1 - i]
+
+        if text_char != suffix_char:
+            ends_with_suffix = False
+            break
+
+print(f"End with suffix? {ends_with_suffix}")
+
 
