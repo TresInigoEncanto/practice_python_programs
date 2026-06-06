53116b79e519896b9a2b59abb6fa3f016ea59d82
Tres <encantotresinigo@gmail.com>
2026-06-06 23:16:03 +0800

    Recreated the lower() function by using the ASCII values and using a for loop together with an if-else statement. Uesd + 32 for the difference of the lowercase and uppercase values in the ASCII

---
+++ batch_6_lower_function.py
@@ -0,0 +1,9 @@
+text = input("Enter input: ")
+result = ""
+
+for char in text:
+    if 'A' <= char <= 'Z':
+        result += chr(ord(char) + 32)
+    else:
+        result += char
+print(result)           
