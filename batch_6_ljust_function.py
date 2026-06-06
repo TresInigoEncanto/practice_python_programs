477800c307614474745b854bd9103bbea3401adf
Tres <encantotresinigo@gmail.com>
2026-06-07 00:10:39 +0800

    Created an if statement to check if the target width is greater than the current length of the user input. Created a variable to add how many padding characters are needed. Created a for loop to append the character padding to the text based on the result of the needed padding

--- batch_6_ljust_function.py
+++ batch_6_ljust_function.py
@@ -7,3 +7,11 @@ target_width = int(width_input)
 current_len = len(user_input)
 
 padded_text = user_input
+
+if target_width > current_len:
+    padding_needed = target_width - current_len
+
+    for i in range(padding_needed):
+        padded_text = padded_text + fill_char
+
+print(f"Result: [{padded_text}]")
