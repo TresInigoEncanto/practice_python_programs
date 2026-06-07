cf8da09181e3b92b2ef6ed3c275b160fabfa6d37
Tres <encantotresinigo@gmail.com>
2026-06-08 03:17:00 +0800

    Created a for loop to process each word. Nested an if-else statement to handle multiple user input spaces and isolate the first character to the rest of the word. Created variables to change the casing of the characters by using upper and lower functions

--- batch_6_title_function.py
+++ batch_6_title_function.py
@@ -5,3 +5,13 @@ if user_input == "":
 
 else:
     word_list = user_input.split()
+
+    words_checked = []
+
+    for word in word_list:
+        if word == "":
+            words_checked.append("")
+        
+        else:
+            first_char = word[0]
+            remaining_char = word[1:]
