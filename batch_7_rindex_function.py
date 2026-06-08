839587e2d615d25e581d5fb415786048cfd165c9
Tres <encantotresinigo@gmail.com>
2026-06-08 20:29:32 +0800

    Created a for loop where it loops backward from the last starting index down to 0. Created a variable to slice a part of the user input equal to the length of the substring

--- batch_7_rindex_function.py
+++ batch_7_rindex_function.py
@@ -10,3 +10,6 @@ if sub_len == 0:
     found_index = input_len
 else:
     start_search = input_len - sub_len
+
+    for i in range(start_search, -1, -1):
+        current_slice = user_input[i: i + sub_len]
