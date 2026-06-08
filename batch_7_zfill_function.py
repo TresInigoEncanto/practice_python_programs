99c25d228fa7b4518e6ef91ddb7c3863f73abe9a
Tres <encantotresinigo@gmail.com>
2026-06-08 17:51:44 +0800

    Recreating zfill() program by having a variable to check if the string starts with a sign first

---
+++ batch_7_zfill_function.py
@@ -0,0 +1,8 @@
+user_input = input("Enter text: ")
+width_input = input("Enter desired width: ")
+
+desired_width = int(width_input)
+current_len = len(user_input)
+
+sign = ""
+clean_text = user_input
