335029e0886d0de3e12fe991b018138108e3d42f
Tres <encantotresinigo@gmail.com>
2026-06-08 18:31:01 +0800

    Created a while loop until it reaches the point where subtraction can no longer fit. Created a variable that gets a slice of the text that is equal to the length of the substring

--- batch_7_count_function.py
+++ batch_7_count_function.py
@@ -9,4 +9,7 @@ match_count = 0
 if string_len == 0:
     match_count = input_len + 1
 else:
-    i = 0
+    index = 0
+
+    while index <= input_len - string_len:
+        current_slice = user_input[index : index + string_len]
