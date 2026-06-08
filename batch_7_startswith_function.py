e87ddfa9e226ebf1078dd76720cc6753b00ff3cd
Tres <encantotresinigo@gmail.com>
2026-06-08 15:04:03 +0800

    Added a variable that assumes the string starts with the prefix unless proven otherwise. Created an if-else statement with a nested for loop to loop from the start of the string until the end of the prefix. Created two variables to compare the characters with each other. Created an if statement to flip to false if the characters did not match even if they are in the same length

--- batch_7_startswith_function.py
+++ batch_7_startswith_function.py
@@ -3,3 +3,18 @@ find_prefix = input("Enter prefix to check: ")
 
 text_len = len(user_input)
 prefix_len = len(find_prefix)
+
+starts_with_prefix = True
+
+if prefix_len > text_len:
+    starts_with_prefix = False
+
+else:
+    for i in range(prefix_len):
+        text_char = user_input[i]
+        prefix_char = find_prefix[i]
+
+        if text_char != prefix_char:
+            starts_with_prefix = False
+
+print(f"Starts with prefix? {starts_with_prefix}")
