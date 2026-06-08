5b2008d413cc161d2f45b1294170b6ea2ae86c66
Tres <encantotresinigo@gmail.com>
2026-06-08 18:54:24 +0800

    Created an if-else statement to increase the match count variable if it matches. Used the addition assignment operator on the index and the string count variable to move the index forward

--- batch_7_count_function.py
+++ batch_7_count_function.py
@@ -2,7 +2,7 @@ user_input = input("Enter text: ")
 string_count = input("Enter substring to count: ")
 
 input_len = len(user_input)
-string_len = len(user_input)
+string_len = len(string_count)
 
 match_count = 0
 
@@ -13,3 +13,11 @@ else:
 
     while index <= input_len - string_len:
         current_slice = user_input[index : index + string_len]
+
+        if current_slice == string_count:
+            match_count += 1
+            index += string_len
+        else:
+            index += 1
+
+print(f"{string_count} appears {match_count} times")
