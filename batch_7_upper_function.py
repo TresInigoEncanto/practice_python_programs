ae4494d18da1a20cb6d7175ec1d8db564558653a
Tres <encantotresinigo@gmail.com>
2026-06-08 14:39:08 +0800

    Recreated the upper() function by creating a for loop that converts the characters into ASCII values and subtract them to their uppercase counterpart to be able to convert them into uppercase characters

---
+++ batch_7_upper_function.py
@@ -0,0 +1,11 @@
+user_input = input("Enter text: ")
+result = ""
+
+for char in user_input:
+    if 'a' <= char <= 'z':
+        result += chr(ord(char) - 32)
+    else:
+        result += char
+
+print(result)
+    
